7e31d63749eb99f39fe5b6dc2429259ad94488ba
Schubert Araujo <xubsoldier@gmail.com>
2024-03-02 21:22:22 -0300

    Menu implementado

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './style.css';
+import imgLogo from '../../imgs/extension.png';
 
 export function Menu(): React.JSX.Element {
   return (
     <div className="menu-top">
       <div className="logo">
+        <img src={imgLogo} alt="logotipo em formato de quebra-cabeça" />
         <p>PUZZLE GAME</p>
       </div>
       <nav>
         <ul className="menu-style">
-          <li>Proposta</li>
-          <li>Mecânica</li>
-          <li>Jogo</li>
+          <li>PROPOSTA</li>
+          <li>MECANICA</li>
+          <li>JOGAR</li>
         </ul>
       </nav>
     </div>
